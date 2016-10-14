645a36d454197ca652af66a37bb1b221ff5dc756
PabloArellanoEstrada <pabloarellanoestrada@gmail.com>
2016-10-14 09:41:32 -0600

    Alarma Emergencia

--- Alarma.java
+++ Alarma.java
@@ -2,12 +2,12 @@ public class Alarma extends Timbre {
 	public void m1() {
 		System.out.println("alarma 1");
 	}
-	/** 
+	
 	public void m2() {
 		super.m1();
 	}
 	 
 	public String toString() {
 		return super.toString() + "..." + super.toString();
-	}*/
+	}
 }
--- AlarmaEmergencia.java
+++ AlarmaEmergencia.java
@@ -1,4 +1,15 @@
-
-public class AlarmaEmergencia {
-
+public class AlarmaEmergencia extends Alarma {
+	public void m1() {
+		System.out.println("emergencia 1");
+	}
+	
+	public void m2() {
+		super.m1();
+		System.out.print(" / ");
+		super.m2();
+	}
+	 
+	public String toString() {
+		return "Emergencia " + super.toString();
+	}
 }
